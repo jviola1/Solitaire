0583424d5b02a318f2700eb48a8e262219fcc2e9
Amritpal1 <asingh59@nyit.edu>
2016-04-27 00:28:41 -0400

    Added function to the Exit button: Dialog box to exit the app or not.

--- app/src/main/java/com/example/jimmy/solitaire/MainActivity.java
+++ app/src/main/java/com/example/jimmy/solitaire/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.jimmy.solitaire;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Rect;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -14,35 +17,40 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+import android.content.DialogInterface;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-        Button play, manual, score, exit;
-        public static final int PLAY_REQUEST_CODE = 0;
-        public static final int MANUAL_REQUEST_CODE = 1;
-        public static final int SCORE_REQUEST_CODE = 2;
+    Button play, manual, score, exit, watch;
+    public static final int PLAY_REQUEST_CODE = 0;
+    public static final int MANUAL_REQUEST_CODE = 1;
+    public static final int SCORE_REQUEST_CODE = 2;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            play = (Button)findViewById(R.id.button);
-            play.setOnClickListener(this);
+        play = (Button) findViewById(R.id.button);
+        play.setOnClickListener(this);
 
-            manual = (Button)findViewById(R.id.button1);
-            manual.setOnClickListener(this);
+        manual = (Button) findViewById(R.id.button1);
+        manual.setOnClickListener(this);
 
-            score = (Button)findViewById(R.id.button3);
-            score.setOnClickListener(this);
+        score = (Button) findViewById(R.id.button3);
+        score.setOnClickListener(this);
 
-            exit = (Button)findViewById(R.id.button4);
-            exit.setOnClickListener(this);
+        exit = (Button) findViewById(R.id.button4);
+        exit.setOnClickListener(this);
 
-        }
+        watch = (Button) findViewById(R.id.button4);
+        watch.setOnClickListener(this);
+
+    }
+
     //    public OnClickListener listen = new OnClickListener() {
-            public void onClick(View v) {
+    public void onClick(View v) {
 
                 /* FIRST WAY
                     Intent i = new Intent(MainActivity.this, PlayActivity.class);
@@ -68,40 +76,102 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 */
 
                 /*SECOND WAY */
-                switch (v.getId()) {
-                    case R.id.button:
-                        Intent i = new Intent(this, Main2Activity.class);
-                        startActivity(i);
+        switch (v.getId()) {
+            case R.id.button:
+                Intent i = new Intent(this, Main2Activity.class);
+                startActivity(i);
 
-                        Toast.makeText(getApplicationContext(), "Let The Games Begin!", Toast.LENGTH_SHORT).show();
-                        break;
+                Toast.makeText(getApplicationContext(), "Let The Games Begin!", Toast.LENGTH_SHORT).show();
+                break;
 
-                    case R.id.button1:
-                        Intent m = new Intent(this, SecondActivity.class);
-                        startActivityForResult(m, MANUAL_REQUEST_CODE);
+            case R.id.button1:
+                Intent m = new Intent(this, SecondActivity.class);
+                startActivityForResult(m, MANUAL_REQUEST_CODE);
 
-                        Toast.makeText(getApplicationContext(), "Study The Rules", Toast.LENGTH_SHORT).show();
-                        break;
+                Toast.makeText(getApplicationContext(), "Study The Rules", Toast.LENGTH_SHORT).show();
+                break;
 
-                    case R.id.button3:
-                        Intent myintent = new Intent(MainActivity.this, ScoreActivity.class);
-                        startActivityForResult(myintent, SCORE_REQUEST_CODE);
+            case R.id.button3:
+                Intent myintent = new Intent(MainActivity.this, ScoreActivity.class);
+                startActivityForResult(myintent, SCORE_REQUEST_CODE);
 
-                        Toast.makeText(getApplicationContext(), "What's Your Score", Toast.LENGTH_SHORT).show();
-                        break;
+                Toast.makeText(getApplicationContext(), "What's Your Score", Toast.LENGTH_SHORT).show();
+                break;
 
-                    case R.id.button4:
-                        Toast.makeText(getApplicationContext(), "Nothing Yet!", Toast.LENGTH_SHORT).show();
-                        break;
+            case R.id.button4:
+                onBackPressed();
+                break;
 
-                    default:
-                        break;
+//            case R.id.button5:
+//                Intent w = new Intent(this, WatchActivity.class);
+//                startActivity(w);
+//                break;
+
+            default:
+                break;
 
 
-    //    };
+            //    };
 
 
-                }}
+        }
+    }
+
+    //    Pressing phone back arrow Twice will exit the game
+//    boolean backToExit = false;
+    @Override
+    public void onBackPressed() {
+        /*  *****
+            Better Way to Exit the app with DialogBox below
+            *****
+         */
+        onBackPressedHandler();
+        return;
+
+        /*  *****
+            Simple Way to Exit the app below
+            *****
+        */
+//        if (backToExit) {
+//            super.onBackPressed();
+//            return;
+//        }
+//        this.backToExit  = true;
+//        Toast.makeText(this, "Click BACK again to exit", Toast.LENGTH_SHORT).show();
+//
+//        new Handler().postDelayed(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//                backToExit =false;
+//            }
+//        }, 2000);
+
+    }
+    //Handles the event when exit in the app and back on the phone is pressed
+    public void onBackPressedHandler() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+//        Dialog Title
+        alertDialog.setTitle("Exit Solitaire");
+//        Dialog Message
+        alertDialog.setMessage("Are you sure you want to exit Solitaire?");
+//        "Yes" Button
+        alertDialog.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        Toast.makeText(getApplicationContext(), "BYE!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+//        "No" Button
+        alertDialog.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        alertDialog.show();
 
 
     }
+}
